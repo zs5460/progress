8e58dfced320213f61fefb81175bb5edada57ab7
zs5460 <zs5460@gmail.com>
2020-09-11 20:15:37 +0800

    opt

--- progress.go
+++ progress.go
@@ -26,19 +26,11 @@ func (p *Progress) GetPercent() int {
 	return int(float32(p.current) / float32(p.total) * 100)
 }
 
-func (p *Progress) Add(i int) {
-
-	p.current += i
-
-	if p.current > p.total {
-		return
-	}
-
+func (p *Progress) Incr() {
+	p.current++
 	p.percent = p.GetPercent()
 	p.rate = strings.Repeat(p.graph, p.percent/2)
-
 	fmt.Printf("\r[%-50s]%4d%% %8d/%d", p.rate, p.percent, p.current, p.total)
-
 	if p.current == p.total {
 		p.Done()
 	}
@@ -46,10 +38,6 @@ func (p *Progress) Add(i int) {
 
 func (p *Progress) SetGraph(s string) {
 	p.graph = s
-}
-
-func (p *Progress) Incr() {
-	p.Add(1)
 }
 
 func (p *Progress) Done() {
--- progress_test.go
+++ progress_test.go
@@ -8,27 +8,19 @@ import (
 func TestRun(t *testing.T) {
 	count := 21
 	bar := New(count)
-	//bar.SetGraph("▫")
 
-	for i := 0; i <= count; i++ {
+	for i := 0; i < count; i++ {
 		time.Sleep(10 * time.Millisecond)
 		bar.Incr()
 	}
 
 	count = 211
 	bar = New(count)
+	bar.SetGraph("▫")
 
-	for i := 0; i <= count; i++ {
+	for i := 0; i < count; i++ {
 		time.Sleep(1 * time.Millisecond)
 		bar.Incr()
-	}
-
-	count = 100
-	bar = New(count)
-
-	for i := 0; i <= count; i++ {
-		time.Sleep(1 * time.Millisecond)
-		bar.Add(33)
 	}
 
 }
